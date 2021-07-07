936b35862da082aceab90e7a918be359d04c2d1b
Manoshiii <manoshi.turjo@northsouth.edu>
2021-07-08 01:55:13 +0600

    Enhancement#3 used Intent for button01 in MainActivity.java If Text One is pressed then it will show that text it contained and in MainActivity.java file contain the changes for the link with Intent method. It will show the next page passage.

--- Lab/Lab02/app/src/main/java/com/example/lab02_task/MainActivity.java
+++ Lab/Lab02/app/src/main/java/com/example/lab02_task/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.lab02_task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b1,b2,b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=findViewById(R.id.bt1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(in);
+            }
+        });
     }
 }
