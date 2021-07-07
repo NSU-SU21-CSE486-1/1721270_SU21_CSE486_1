6d27363b04cd704190d3874b556cbae5aff7942a
Manoshiii <manoshi.turjo@northsouth.edu>
2021-07-08 00:15:36 +0600

    Enhancement#3 Button01 Added One button added with proper attributes. textsize, textstyle, textcolor, background have added to Button class. orientation and gravity have also added to LinearLayout.

---
+++ Lab/Lab02/app/src/main/java/com/example/lab02_task/Main4Activity.java
@@ -0,0 +1,14 @@
+package com.example.lab02_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
