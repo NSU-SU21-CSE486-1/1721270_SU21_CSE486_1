45b82d7d1c2fff6af472feb1361d743b5a0dad72
Manoshiii <manoshi.turjo@northsouth.edu>
2021-07-25 04:08:36 +0600

    enhancement #10 onclick method name changed onclick method name has been changed according to the id name.

--- Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/ThirdActivity.java
+++ Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/ThirdActivity.java
@@ -2,7 +2,11 @@ package com.manoshi.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(UniversityAffiliation.MSG);
+        TextView textView = findViewById(R.id.textView6);
+        textView.setText(message);
     }
 }
