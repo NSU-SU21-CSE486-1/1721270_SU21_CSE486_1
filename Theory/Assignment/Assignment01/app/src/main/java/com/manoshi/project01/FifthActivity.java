d72fe292b3735d15b67c8745e64140cdf00f2881
Manoshiii <manoshi.turjo@northsouth.edu>
2021-07-25 22:09:53 +0600

    enhancement #16 message retrieved in FifthActivity.java for showing all information intent added to retrieve info.

--- Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/FifthActivity.java
+++ Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/FifthActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FifthActivity extends AppCompatActivity {
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
+        message = getIntent().getStringExtra(ForthActivity.MSG);
     }
 }
