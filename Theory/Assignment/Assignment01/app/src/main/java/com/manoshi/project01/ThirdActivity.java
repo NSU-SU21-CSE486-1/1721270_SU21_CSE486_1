d02486f2105123e91d8c4b4f303151d22fcbf1ab
Manoshiii <manoshi.turjo@northsouth.edu>
2021-07-25 20:56:33 +0600

    enhancement #12 added buttonNext for fourth page in ThirdActivity.java file added buttonNext and intent added.

--- Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/ThirdActivity.java
+++ Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/ThirdActivity.java
@@ -4,19 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class ThirdActivity extends AppCompatActivity {
+    public static final String MSG = "com.manoshi.project01.MSG";
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(UniversityAffiliation.MSG);
+        message = intent.getStringExtra(UniversityAffiliation.MSG);
         TextView textView = findViewById(R.id.textView6);
         textView.setText(message);
+    }
+    public void buttonNext (View view) {
+        Intent intent = new Intent(this, ForthActivity.class);
+        intent.putExtra(MSG, message);
+        startActivity(intent);
     }
 }
