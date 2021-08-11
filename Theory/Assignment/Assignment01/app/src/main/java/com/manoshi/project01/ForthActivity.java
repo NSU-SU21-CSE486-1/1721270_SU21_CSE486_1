4023f6c28d67d356221ea9015c4a24059d95fbac
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-12 02:51:50 +0600

    enhancement #19 initial project created For Lab05 a project has been created with api16 and gitignore file added.

--- Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/ForthActivity.java
+++ Theory/Assignment/Assignment01/app/src/main/java/com/manoshi/project01/ForthActivity.java
@@ -2,8 +2,11 @@ package com.manoshi.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class ForthActivity extends AppCompatActivity {
     public static final String MSG = "com.manoshi.project01.MSG";
@@ -14,7 +17,15 @@ public class ForthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forth);
     }
-    public void buttonNext(View view) {
-
+    public void buttonNext (View view) {
+        Intent intent = new Intent(this, FifthActivity.class);
+        EditText editText1 = findViewById(R.id.editTextTextPersonName2);
+        EditText editText2 = findViewById(R.id.editTextTextPersonName3);
+        message = getIntent().getStringExtra(ThirdActivity.MSG);
+        message += "\nEmail: " + editText1.getText().toString() + "\nPhone: " +
+                editText2.getText().toString() ;
+        intent.putExtra(MSG, message);
+        startActivity(intent);
     }
+
 }
