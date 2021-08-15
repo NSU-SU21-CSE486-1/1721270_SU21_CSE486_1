c4d83b6c4aafc17e663598c61b83c90af46faf54
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-15 23:11:01 +0600

    enhancement #31 CreateNotification class added condition for CreateNotification added

--- Lab/Lab05/app/src/main/java/com/manoshi/lab05/CreateNotification.java
+++ Lab/Lab05/app/src/main/java/com/manoshi/lab05/CreateNotification.java
@@ -1,16 +1,24 @@
 package com.manoshi.lab05;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
+
+import androidx.core.app.NotificationManagerCompat;
 
 public class CreateNotification {
     public static final String CHANNEL_ID = "channel11";
 
     public static final String ACTIONPREVIOUS = "actionprevious";
     public static final String CHANNEL_PLAY = "actionplay";
-    public static final String CHANNEL_NEXT = "actiionnext";
+    public static final String CHANNEL_NEXT = "actionnext";
 
     public static Notification notification;
-    public static void createNotification(Context context, )
+    public static void createNotification(Context context, Track track, int playbutton, int pos, int size){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        }
+    }
 
 }
