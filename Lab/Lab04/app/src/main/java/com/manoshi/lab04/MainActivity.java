cc68137f362d7bf2649dfe808856c3e161cd8fc8
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-15 13:41:56 +0600

    enhancement #24 showDateDialog added ShowDateDialog method added for viewing date.

--- Lab/Lab04/app/src/main/java/com/manoshi/lab04/MainActivity.java
+++ Lab/Lab04/app/src/main/java/com/manoshi/lab04/MainActivity.java
@@ -2,8 +2,14 @@ package com.manoshi.lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     EditText date_in;
@@ -16,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         date_in=findViewById(R.id.date_input);
         time_in=findViewById(R.id.time_input);
+        date_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog(date_in)
+            }
+        });
+    }
+
+    private void showDateDialog(EditText date_in) {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+            }
+        };
+        new DatePickerDialog(MainActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
