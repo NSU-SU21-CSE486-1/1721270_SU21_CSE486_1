3c41704e767c368fcb1454e249ad0a3ef41732fb
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-15 22:59:03 +0600

    enhancement #31 creating variable created four variables and a notification variable.

--- Lab/Lab05/app/src/main/java/com/manoshi/lab05/CreateNotification.java
+++ Lab/Lab05/app/src/main/java/com/manoshi/lab05/CreateNotification.java
@@ -1,4 +1,16 @@
 package com.manoshi.lab05;
 
+import android.app.Notification;
+import android.content.Context;
+
 public class CreateNotification {
+    public static final String CHANNEL_ID = "channel11";
+
+    public static final String ACTIONPREVIOUS = "actionprevious";
+    public static final String CHANNEL_PLAY = "actionplay";
+    public static final String CHANNEL_NEXT = "actiionnext";
+
+    public static Notification notification;
+    public static void createNotification(Context context, )
+
 }
