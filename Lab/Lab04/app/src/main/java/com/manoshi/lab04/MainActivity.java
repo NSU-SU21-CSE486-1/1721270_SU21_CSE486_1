43506e23e0075d97d7dc3cbfbdc14d485ce490a2
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-15 15:26:12 +0600

    enhancement #26 added type of the variables. edited typenull for viewing hints while tying.

--- Lab/Lab04/app/src/main/java/com/manoshi/lab04/MainActivity.java
+++ Lab/Lab04/app/src/main/java/com/manoshi/lab04/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
@@ -26,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         date_in=findViewById(R.id.date_input);
         time_in=findViewById(R.id.time_input);
         date_time_in=findViewById(R.id.date_time_input);
+
+        date_time_in.setInputType(InputType.TYPE_NULL);
+        date_in.setInputType(InputType.TYPE_NULL);
+        time_in.setInputType(InputType.TYPE_NULL);
         date_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,7 +56,19 @@ public class MainActivity extends AppCompatActivity {
         DatePickerDialog.OnDateSetListener dateSetListener= new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        calendar.set(Calendar.MINUTE,minute);
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
+                    }
+                };
+                new TimePickerDialog(MainActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE), false).show();
             }
         };
         new DatePickerDialog(MainActivity.this, dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
@@ -65,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
                 calendar.set(Calendar.MINUTE,minute);
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
-                time_in.setText(simpleDateFormat.format(calendar));
+                time_in.setText(simpleDateFormat.format(calendar.getTime()));
 
             }
         };
@@ -82,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 calendar.set(Calendar.MONTH,month);
                 calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd");
-                date_in.setText(simpleDateFormat.format(calendar));
+                date_in.setText(simpleDateFormat.format(calendar.getTime()));
 
             }
         };
