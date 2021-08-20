2e0f489f384adb0c738b15fc5b712dfeeaf4a5d9
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-20 20:13:20 +0600

    enhancement #33 UniversityAffiliation.java file created added another class named UniversityAffiliation.java

--- Theory/Quiz/Quiz02/app/src/main/java/com/manoshi/project02/UniversityAffiliation.java
+++ Theory/Quiz/Quiz02/app/src/main/java/com/manoshi/project02/UniversityAffiliation.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class BasicInformation extends Fragment {
+public class UniversityAffiliation extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
