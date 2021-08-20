0d2903498134a81e31850d0e6e3db14faf53fa6c
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-20 20:33:50 +0600

    enhancement #35 Others.java file created added another class named Others.java

--- Theory/Quiz/Quiz02/app/src/main/java/com/manoshi/project02/Others.java
+++ Theory/Quiz/Quiz02/app/src/main/java/com/manoshi/project02/Others.java
@@ -9,10 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class UniversityAffiliation extends Fragment {
+public class Others extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2_layout, container, false);
+        return inflater.inflate(R.layout.fragment3_layout, container, false);
     }
 }
