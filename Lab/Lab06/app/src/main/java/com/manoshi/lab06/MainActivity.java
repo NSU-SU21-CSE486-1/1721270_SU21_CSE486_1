c1fe1393747beda84f6aec5d6bb7e947de8771f9
Manoshiii <manoshi.turjo@northsouth.edu>
2021-08-25 23:02:24 +0600

    enhancement #38 MainActivity.java edited variables added in MainActivity.java.

--- Lab/Lab06/app/src/main/java/com/manoshi/lab06/MainActivity.java
+++ Lab/Lab06/app/src/main/java/com/manoshi/lab06/MainActivity.java
@@ -2,9 +2,21 @@ package com.manoshi.lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private int mColor;
+    private TextView mShowCountTextView;
+    private final String COUNT_KEY = "count";
+    private final String COLOR_KEY = "color";
+    private final String COUNT_SAVE_KEY = "count_save";
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile = "com.manoshi.lab06";
+    private String COUNT_EXTRA = "count_extra";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
