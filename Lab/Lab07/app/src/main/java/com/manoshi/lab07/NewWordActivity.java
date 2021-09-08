9ab46c7ffbc98932f85a5e5f221b5f2b87812459
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-08 13:54:39 +0600

    enhancement #42 oncreate method added onceated method added in NewWordActivity.java.

--- Lab/Lab07/app/src/main/java/com/manoshi/lab07/NewWordActivity.java
+++ Lab/Lab07/app/src/main/java/com/manoshi/lab07/NewWordActivity.java
@@ -6,14 +6,27 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 public class NewWordActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY = "com.manoshi.lab07";
-    public static final String EXTRA_REPLY_ID = "com.manoshi.lab07";
+    public static final String EXTRA_REPLY = "com.manoshi.lab07.REPLY";
+    public static final String EXTRA_REPLY_ID = "com.manoshi.lab07.REPLY_ID";
 
     private EditText mEditWordView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
+
+        mEditWordView = findViewById(R.id.edit_word);
+        int id = -1;
+
+        final Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String word = extras.getString(EXTRA_DATA_UPDATE_WORD, "");
+            if (!word.isEmpty()) {
+                mEditWordView.setText(word);
+                mEditWordView.setSelection(word.length());
+                mEditWordView.requestFocus();
+            }
+        }
     }
 }
