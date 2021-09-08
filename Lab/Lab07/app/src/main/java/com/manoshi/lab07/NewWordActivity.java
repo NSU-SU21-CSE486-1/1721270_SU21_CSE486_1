b09c1f9dff1441555dad437cb0a425c75fd9837d
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-08 13:38:16 +0600

    enhancement #42 string added edittext added.

--- Lab/Lab07/app/src/main/java/com/manoshi/lab07/NewWordActivity.java
+++ Lab/Lab07/app/src/main/java/com/manoshi/lab07/NewWordActivity.java
@@ -3,8 +3,13 @@ package com.manoshi.lab07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NewWordActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.manoshi.lab07";
+    public static final String EXTRA_REPLY_ID = "com.manoshi.lab07";
+
+    private EditText mEditWordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
