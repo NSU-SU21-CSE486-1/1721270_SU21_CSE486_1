eac798a02d57cc2bcb5f116fc047bc92f792299a
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-08 13:58:27 +0600

    enhancement #42 method added setonclik method added.

--- Lab/Lab07/app/src/main/java/com/manoshi/lab07/NewWordActivity.java
+++ Lab/Lab07/app/src/main/java/com/manoshi/lab07/NewWordActivity.java
@@ -2,7 +2,11 @@ package com.manoshi.lab07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class NewWordActivity extends AppCompatActivity {
@@ -28,5 +32,32 @@ public class NewWordActivity extends AppCompatActivity {
                 mEditWordView.requestFocus();
             }
         }
+        final Button button = findViewById(R.id.button_save);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Create a new Intent for the reply.
+                Intent replyIntent = new Intent();
+                if (TextUtils.isEmpty(mEditWordView.getText())) {
+                    // No word was entered, set the result accordingly.
+                    setResult(RESULT_CANCELED, replyIntent);
+                } else {
+                    // Get the new word that the user entered.
+                    String word = mEditWordView.getText().toString();
+                    // Put the new word in the extras for the reply Intent.
+                    replyIntent.putExtra(EXTRA_REPLY, word);
+                    if (extras != null && extras.containsKey(EXTRA_DATA_ID)) {
+                        int id = extras.getInt(EXTRA_DATA_ID, -1);
+                        if (id != -1) {
+                            replyIntent.putExtra(EXTRA_REPLY_ID, id);
+                        }
+                    }
+                    // Set the result status to indicate success.
+                    setResult(RESULT_OK, replyIntent);
+                }
+                finish();
+            }
+        });
     }
 }
+
