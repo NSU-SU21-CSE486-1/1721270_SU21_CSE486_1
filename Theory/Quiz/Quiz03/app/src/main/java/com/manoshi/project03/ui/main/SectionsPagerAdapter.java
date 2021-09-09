535e9e97a27092da14c8a6b46c6e10c868296057
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-10 01:26:38 +0600

    enhancement #44 sectionpager file edited. three case added here and return fragment.

--- Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/ui/main/SectionsPagerAdapter.java
+++ Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/ui/main/SectionsPagerAdapter.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.manoshi.project03.Fragment1;
+import com.manoshi.project03.Fragment2;
+import com.manoshi.project03.Fragment3;
 import com.manoshi.project03.R;
 
 /**
@@ -27,9 +30,19 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch ( position) {
+            case 0:
+                fragment = new Fragment1();
+                break;
+            case 1:
+                fragment = new Fragment2();
+                break;
+            case 2:
+                fragment = new Fragment3();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
