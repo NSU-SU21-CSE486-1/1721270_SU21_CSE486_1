390694b1da23fa22ba5fc7e0823717151aa77723
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-10 01:02:02 +0600

    enhancement #44 Fragment2.java file created. another java file Fragment2.java file created.

--- Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/Fragment2.java
+++ Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/Fragment2.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Fragment1 extends Fragment {
+public class Fragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
