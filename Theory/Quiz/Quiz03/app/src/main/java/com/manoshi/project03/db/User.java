cad02ce1a88032a51fe64fecf10c28e36ff2e09f
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-10 20:51:50 +0600

    enhancement #45 User.java class edited. two columninfo added in this class.

--- Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/User.java
+++ Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/User.java
@@ -1,5 +1,6 @@
 package com.manoshi.project03.db;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -7,6 +8,10 @@ import androidx.room.PrimaryKey;
 public class User {
     @PrimaryKey(autoGenerate = true)
     public int uid;
+
+    @ColumnInfo(name = "first_name")
     public String firstname;
+
+    @ColumnInfo(name = "Last_name")
     public String lastname;
 }
