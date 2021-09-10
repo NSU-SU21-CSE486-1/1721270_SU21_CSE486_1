c2426aba52e346d1361e9a98a0d78d25564610fe
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-10 20:48:32 +0600

    enhancement #45 User.java class edited. three variables added in this class.

--- Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/User.java
+++ Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/User.java
@@ -1,4 +1,12 @@
 package com.manoshi.project03.db;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+    public String firstname;
+    public String lastname;
 }
