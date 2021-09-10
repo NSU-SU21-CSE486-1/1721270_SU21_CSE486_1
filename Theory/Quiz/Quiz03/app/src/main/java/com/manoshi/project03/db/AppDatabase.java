ce547edd28151dbb6624ebbe70b16cd82db89d02
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-10 20:40:04 +0600

    enhancement #45 AppDatabase.java class edited. class and new variables are decleared here.

--- Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/AppDatabase.java
+++ Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/AppDatabase.java
@@ -1,4 +1,20 @@
 package com.manoshi.project03.db;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+    private static AppDatabase INSTANCE;
+    public static AppDatabase getInstance(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "DB Name")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+    }
 }
