d5eb0620df4d5bea55c6758d53bba5edaa96e176
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-10 21:22:09 +0600

    enhancement #45 xml file edited. attributes added in fragment files.

--- Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/AppDatabase.java
+++ Theory/Quiz/Quiz03/app/src/main/java/com/manoshi/project03/db/AppDatabase.java
@@ -9,9 +9,11 @@ import androidx.room.RoomDatabase;
 @Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
+
     private static AppDatabase INSTANCE;
-    public static AppDatabase getInstance(Context context) {
-        if(INSTANCE == null) {
+
+    public static void getInstance(Context context) {
+        if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "DB Name")
                     .allowMainThreadQueries()
                     .build();
