75b24ed365e81a07155f3e17ae6c51ffa8742ed8
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-16 20:44:40 +0600

    enhancement #47 added four variables. edittext and button added in MainActivity.java file.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/MainActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/MainActivity.java
@@ -3,8 +3,16 @@ package com.manoshi.uniclubz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+
+    private Button btnLogin;
+    private Button btnRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
