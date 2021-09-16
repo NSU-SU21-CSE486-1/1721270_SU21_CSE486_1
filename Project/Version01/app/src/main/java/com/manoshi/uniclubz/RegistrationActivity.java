c31fc8d89e8424d9f7ddb094c4cbe03ece00e09b
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-16 23:20:49 +0600

    enhancement #46 variable added. in RegistrationActivity.java three variables added

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/RegistrationActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/RegistrationActivity.java
@@ -3,8 +3,15 @@ package com.manoshi.uniclubz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText emailReg;
+    private EditText passReg;
+    private Button btnReg;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
