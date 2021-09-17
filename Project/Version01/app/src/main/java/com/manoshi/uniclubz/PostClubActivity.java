1029561eb3fa18cf629e24a10762db49e7ec4dff
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-17 23:13:27 +0600

    enhancement #52 PostClubActivity.java edited. new intent created in this file.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/PostClubActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/PostClubActivity.java
@@ -2,6 +2,7 @@ package com.manoshi.uniclubz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,7 +20,7 @@ public class PostClubActivity extends AppCompatActivity {
         fabBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getApplicationContext(), InsertClubPostActivity.class));
             }
         });
     }
