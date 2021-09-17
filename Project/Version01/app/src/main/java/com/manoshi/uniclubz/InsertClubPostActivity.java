fe4ddffa15b52cac1c0e926dd1ed85f6beaeeaa0
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-18 00:11:06 +0600

    enhancement #53 InsertClubActivity edited. variable added.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/InsertClubPostActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/InsertClubPostActivity.java
@@ -1,14 +1,19 @@
 package com.manoshi.uniclubz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class InsertClubPostActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_club_post);
+
+        toolbar = findViewById(R.id.insert_club_toolbar);
     }
 }
