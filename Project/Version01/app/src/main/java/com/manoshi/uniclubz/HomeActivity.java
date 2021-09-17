b120641c175fa81a6eaaebe1132049c5d0031d8b
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-17 22:05:26 +0600

    enhancement #50 toolbar variable edited.. findviewid added in HomeActivity.java file.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/HomeActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/HomeActivity.java
@@ -20,6 +20,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.toolbar_home);
         btnclub = findViewById(R.id.btn_club);
         btnpost = findViewById(R.id.btn_post);
         btnprofile = findViewById(R.id.btn_profile);
