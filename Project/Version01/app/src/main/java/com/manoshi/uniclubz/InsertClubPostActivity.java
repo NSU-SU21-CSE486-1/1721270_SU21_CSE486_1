4813d4311c6894663c5dcfe2c51f6d96b74fcd96
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-18 00:16:30 +0600

    enhancement #53 InsertClubActivity edited. four variable added.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/InsertClubPostActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/InsertClubPostActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class InsertClubPostActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText event_title;
+    private EditText event_description;
+    private EditText club_name;
+    private EditText event_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
