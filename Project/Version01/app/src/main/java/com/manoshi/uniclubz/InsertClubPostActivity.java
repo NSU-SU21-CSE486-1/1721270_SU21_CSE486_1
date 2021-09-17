a7e0bd8d967ff2b46cbc093ef0bade1385729f57
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-17 23:11:19 +0600

    enhancement #53 new activity added. new activity named InsertClubPostActivity.java created.

---
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/InsertClubPostActivity.java
@@ -0,0 +1,14 @@
+package com.manoshi.uniclubz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertClubPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_club_post);
+    }
+}
