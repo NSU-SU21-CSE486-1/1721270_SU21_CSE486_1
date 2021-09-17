743cd6787943254a9d80dccebcf9a390b7f6403b
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-17 20:41:57 +0600

    enhancement #51 intent passing. in btnprofile, new intent pass by calling function.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/HomeActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/HomeActivity.java
@@ -37,7 +37,7 @@ public class HomeActivity extends AppCompatActivity {
         btnprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
             }
         });
     }
