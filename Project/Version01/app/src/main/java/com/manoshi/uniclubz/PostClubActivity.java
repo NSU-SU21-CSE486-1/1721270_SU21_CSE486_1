53583cfea17eb13a036ea8d84ed043b78cfb3da3
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-17 23:06:53 +0600

    enhancement #52 postclub design variable call for floating action button.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/PostClubActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/PostClubActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class PostClubActivity extends AppCompatActivity {
+    private FloatingActionButton fabBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_club);
+
+        fabBtn = findViewById(R.id.fab_add);
     }
 }
