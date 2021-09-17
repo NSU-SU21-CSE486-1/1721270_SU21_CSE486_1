1b9afebec5b11ede98b9ddeafb98ae3a4424f959
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-18 00:46:30 +0600

    enhancement #54 Data.java file edited. constructor added.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/Model/Data.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/Model/Data.java
@@ -8,4 +8,17 @@ public class Data {
 
     String id;
     String time;
+
+    public Data(){
+
+    }
+
+    public Data(String title, String description, String name, String date, String id, String time) {
+        this.title = title;
+        this.description = description;
+        this.name = name;
+        this.date = date;
+        this.id = id;
+        this.time = time;
+    }
 }
