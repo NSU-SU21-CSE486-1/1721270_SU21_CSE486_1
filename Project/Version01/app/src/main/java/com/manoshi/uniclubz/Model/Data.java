ff02552938608b05620d6f13471d534643cdc905
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-18 00:47:29 +0600

    enhancement #54 Data.java file edited. setter and getter added.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/Model/Data.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/Model/Data.java
@@ -21,4 +21,52 @@ public class Data {
         this.id = id;
         this.time = time;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
