d7d47b2b726d072c6953f93aebccf246d331ffcf
Manoshiii <manoshi.turjo@northsouth.edu>
2021-09-17 20:16:57 +0600

    enhancement #50 three variable added. three variable added in HomeActivity.java file.

--- Project/Version01/app/src/main/java/com/manoshi/uniclubz/HomeActivity.java
+++ Project/Version01/app/src/main/java/com/manoshi/uniclubz/HomeActivity.java
@@ -3,8 +3,12 @@ package com.manoshi.uniclubz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button btnclub;
+    private Button btnpost;
+    private Button btnprofile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
